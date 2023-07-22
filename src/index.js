a5105033a4fff89e6ffc37b3a3b1129ce0c95f5d
Alexander Bascuñan <abascunan@jumpitt.com>
2023-07-21 21:00:27 -0400

    nuevos cambios

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import mapboxgl from 'mapbox-gl'
 
+ // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmFzY3VhbGFyY29uIiwiYSI6ImNsZXI4M25mZjB0bzYzcG52NzFwcmtmbWMifQ.MXM7eFMZIpRjX6654-cg4g';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
